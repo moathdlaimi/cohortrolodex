b657964368a071479d98e779a40ddc656940605a
Moath <moath.dlaii@gmail.com>
2020-06-18 15:16:33 -0700

    commented the get route to test heroku

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.render('index.html')
-})
+// app.get('/', (req,res) => {
+//     res.render('index.html')
+// })
 
 app.listen(PORT, () => {
   console.log('Listening to port', PORT);
