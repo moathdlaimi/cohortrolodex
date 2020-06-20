54ccdad29484fef493cb8a93c09fe8254cbaa5c5
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-06-20 14:49:26 -0700

    restructed card view and added condtional logic to control form visibility

--- models/users.js
+++ models/users.js
@@ -2,20 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {type:String, unique:true, required:true},
-  password: {type:String, required:true},
-  fName:{type:String, required:true},
-  lName:{type:String, required:true},
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  fName: { type: String, required: true },
+  lName: { type: String, required: true },
+  profilePic: { type: String, required: true },
   contact: {
-    github:String,
-    linkedin:String,
-    website:String, 
+    github: String,
+    linkedin: String,
+    website: String,
   },
-  skills:[{type:String}],
-  location:String,
-  brand:String,
-
-
+  skills: [{ type: String }],
+  location: String,
+  brand: String,
 });
 
 const Users = mongoose.model("user", userSchema);
