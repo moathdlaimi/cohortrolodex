4fda1d2aa1339e1be75c2ee30692cb464889b7a2
Moath <moath.dlaii@gmail.com>
2020-06-20 11:43:29 -0700

    added the favicon middleware

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.use(
   })
 );
 
+app.use( function(req, res, next) {
+
+  if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+
+});
+
 //=============
 //CONTROLLERS
 //=============
